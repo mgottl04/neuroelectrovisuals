6e0e6281da618cf7f72c24fe8475a8bc08cb4227
Michael Gottlieb <Michael Gottlieb>
2015-11-19 10:56:24 -0800

    Simplified UI by making adding input selectors a function.

--- ui.R
+++ ui.R
@@ -1,6 +1,22 @@
 library(shiny)
 library(ggvis)
 # Define UI for application that draws a histogram
+
+add_input_selector <- function(x_label,y_label){
+  fluidRow(
+    selectInput(x_label, 
+                label = "Choose a variable to display on x axis",
+                choices =names(bigData),
+                selected = names(bigData)[[12]]
+    ),
+    selectInput(y_label, 
+                label = "Choose a variable to display on y axis",
+                choices =names(bigData),
+                selected = names(bigData)[[2]]
+    )
+  )
+  
+}
 shinyUI(fluidPage(
   
   # Application title
@@ -9,6 +25,7 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(h5('filter menu here')
+                
          
        
     ),
@@ -19,36 +36,14 @@ shinyUI(fluidPage(
         column(6, 
                 actionButton('toggle1','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
                       conditionalPanel(condition = 'input.toggle1 % 2 == 0',
-                                fluidRow(
-                                        selectInput("x1", 
-                                                            label = "Choose a variable to display on x axis",
-                                                            choices =names(bigData),
-                                                            selected = names(bigData)[[12]]
-                                                            ),
-                                         selectInput("y1", 
-                                                            label = "Choose a variable to display on y axis",
-                                                            choices =names(bigData),
-                                                            selected = names(bigData)[[2]]
-                                                    )
-                                       )
+                                add_input_selector('x1','y1')
                                 ),
                conditionalPanel('input.toggle1 % 2 == 1', ggvisOutput("plot1"))
                ),
                column(3,
                       actionButton('toggle2','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
                       conditionalPanel(condition = 'input.toggle2 % 2 == 0',
-                                       fluidRow(
-                                         selectInput("x2", 
-                                                     label = "Choose a variable to display on x axis",
-                                                     choices =names(bigData),
-                                                     selected = names(bigData)[[12]]
-                                         ),
-                                         selectInput("y2", 
-                                                     label = "Choose a variable to display on y axis",
-                                                     choices =names(bigData),
-                                                     selected = names(bigData)[[2]]
-                                         )
-                                       )
+                                       add_input_selector('x2','y2')
                       ),
                       conditionalPanel('input.toggle2 % 2 == 1', ggvisOutput("plot2"))
                )
@@ -57,36 +52,14 @@ shinyUI(fluidPage(
         column(6,
                actionButton('toggle3','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
                conditionalPanel(condition = 'input.toggle3 % 2 == 0',
-                                fluidRow(
-                                  selectInput("x3", 
-                                              label = "Choose a variable to display on x axis",
-                                              choices =names(bigData),
-                                              selected = names(bigData)[[12]]
-                                  ),
-                                  selectInput("y3", 
-                                              label = "Choose a variable to display on y axis",
-                                              choices =names(bigData),
-                                              selected = names(bigData)[[2]]
-                                  )
-                                )
+                                add_input_selector('x3','y3')
                ),
                conditionalPanel('input.toggle3 % 2 == 1', ggvisOutput("plot3"))
         ),
         column(3,
                actionButton('toggle4','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
                conditionalPanel(condition = 'input.toggle4 % 2 == 0',
-                                fluidRow(
-                                  selectInput("x4", 
-                                              label = "Choose a variable to display on x axis",
-                                              choices =names(bigData),
-                                              selected = names(bigData)[[12]]
-                                  ),
-                                  selectInput("y4", 
-                                              label = "Choose a variable to display on y axis",
-                                              choices =names(bigData),
-                                              selected = names(bigData)[[2]]
-                                  )
-                                )
+                                add_input_selector('x4','y4')
                ),
                conditionalPanel('input.toggle4 % 2 == 1', ggvisOutput("plot4"))
         )
