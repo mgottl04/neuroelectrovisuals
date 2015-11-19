0dbe1d7aa4606db6793437f54399f761e4ffb781
Mike <mikemgottlieb@gmail.com>
2015-11-18 20:33:16 -0800

    Made it so four plots appear and UI is reactive. Moved axis selectors out of the sidebar which will eventually be used for the filter menu. The axes still look like crap in a lot of cases and I haven't been able to force a proportional layout on the application yet.

--- ui.R
+++ ui.R
@@ -8,30 +8,89 @@ shinyUI(fluidPage(
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
-    sidebarPanel(
-      fluidRow(
-        selectInput("xaxis", 
-                    label = "Choose a variable to display on x axis",
-                    choices =names(bigData),
-                    selected = names(bigData)[[12]]),
-        selectInput("yaxis", 
-                    label = "Choose a variable to display on y axis",
-                    choices =names(bigData),
-                    selected = names(bigData)[[2]])
-        )
-    
-        
+    sidebarPanel(h5('filter menu here')
+         
+       
     ),
    
     # Show a plot of the generated distribution
-    mainPanel( ggvisOutput("distPlot")
+    mainPanel(
+      fluidRow(div(style = 'height:"50%";'),
+        column(6, 
+                actionButton('toggle1','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
+                      conditionalPanel(condition = 'input.toggle1 % 2 == 0',
+                                fluidRow(
+                                        selectInput("x1", 
+                                                            label = "Choose a variable to display on x axis",
+                                                            choices =names(bigData),
+                                                            selected = names(bigData)[[12]]
+                                                            ),
+                                         selectInput("y1", 
+                                                            label = "Choose a variable to display on y axis",
+                                                            choices =names(bigData),
+                                                            selected = names(bigData)[[2]]
+                                                    )
+                                       )
+                                ),
+               conditionalPanel('input.toggle1 % 2 == 1', ggvisOutput("plot1"))
+               ),
+               column(3,
+                      actionButton('toggle2','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
+                      conditionalPanel(condition = 'input.toggle2 % 2 == 0',
+                                       fluidRow(
+                                         selectInput("x2", 
+                                                     label = "Choose a variable to display on x axis",
+                                                     choices =names(bigData),
+                                                     selected = names(bigData)[[12]]
+                                         ),
+                                         selectInput("y2", 
+                                                     label = "Choose a variable to display on y axis",
+                                                     choices =names(bigData),
+                                                     selected = names(bigData)[[2]]
+                                         )
+                                       )
+                      ),
+                      conditionalPanel('input.toggle2 % 2 == 1', ggvisOutput("plot2"))
+               )
+        ),
+      fluidRow(
+        column(6,
+               actionButton('toggle3','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
+               conditionalPanel(condition = 'input.toggle3 % 2 == 0',
+                                fluidRow(
+                                  selectInput("x3", 
+                                              label = "Choose a variable to display on x axis",
+                                              choices =names(bigData),
+                                              selected = names(bigData)[[12]]
+                                  ),
+                                  selectInput("y3", 
+                                              label = "Choose a variable to display on y axis",
+                                              choices =names(bigData),
+                                              selected = names(bigData)[[2]]
+                                  )
+                                )
+               ),
+               conditionalPanel('input.toggle3 % 2 == 1', ggvisOutput("plot3"))
+        ),
+        column(3,
+               actionButton('toggle4','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
+               conditionalPanel(condition = 'input.toggle4 % 2 == 0',
+                                fluidRow(
+                                  selectInput("x4", 
+                                              label = "Choose a variable to display on x axis",
+                                              choices =names(bigData),
+                                              selected = names(bigData)[[12]]
+                                  ),
+                                  selectInput("y4", 
+                                              label = "Choose a variable to display on y axis",
+                                              choices =names(bigData),
+                                              selected = names(bigData)[[2]]
+                                  )
+                                )
+               ),
+               conditionalPanel('input.toggle4 % 2 == 1', ggvisOutput("plot4"))
+        )
       )
-    )#,
-#     fluidRow(
-#           column(width = 3,
-#              verbatimTextOutput("hover_info")
-#     
-#       )
-    )
-    
-    )
+      )
+      
+      )))
