1071e4e1eb09175b34fb031d5ca9a02598920ec0
Michael Gottlieb <Michael Gottlieb>
2015-11-19 13:04:54 -0800

    changed axis selection

--- server.R
+++ server.R
@@ -7,79 +7,79 @@ library(dplyr)
 shinyServer(function(input, output,session) {
   
   # Expression that generates a histogram. The expression is
-
-
+  
+  
   mtc <- reactive({
     #global filtering will occur in this reactive
     data = bigData #%>% filter(!is.na(JxnOffset))
     data
   })
-
-plot1 <- reactive({
-  plotThing <- reactive({
-    data = mtc()
-    data %>%filter((!is.na(data %>% select(match(input$x1, names(data)))) & (!is.na(data %>% select(match(input$y1, names(data))))))) 
-  })
-  plot <- plotThing %>% 
+  
+  plot1 <- reactive({
+    plotThing <- reactive({
+      data = mtc()
+      data %>%filter((!is.na(data %>% select(match(input$x1, names(data)))) & (!is.na(data %>% select(match(input$y1, names(data))))))) 
+    })
+    plot <- plotThing %>% 
       ggvis(x=as.name(input$x1), y =as.name(input$y1), key:=~Pmid) %>% 
-    layer_points() %>%
-    add_tooltip(function(data){
-      paste0("Pmid: ", as.character(data$Pmid),"<br>",
-             input$x1,": ", as.character(data[[1]]), "<br>", input$y1, ": ", as.character(data[[2]]),"<br>")
-    }, "hover")%>%set_options(renderer = "canvas")
-
-})
-
-plot2 <- reactive({
-  plotThing <- reactive({
-    data = mtc()
-    data %>%filter((!is.na(data %>% select(match(input$x2, names(data)))) & (!is.na(data %>% select(match(input$y2, names(data))))))) 
+      layer_points() %>%
+      add_tooltip(function(data){
+        paste0("Pmid: ", as.character(data$Pmid),"<br>",
+               input$x1,": ", as.character(data[[1]]), "<br>", input$y1, ": ", as.character(data[[2]]),"<br>")
+      }, "hover")%>%set_options(renderer = "canvas")
+    
   })
-  plotThing %>% 
-    ggvis(x=as.name(input$x2), y =as.name(input$y2), key:=~Pmid) %>% 
-    layer_points() %>%
-    add_tooltip(function(data){
-      paste0("Pmid: ", as.character(data$Pmid),"<br>",
-             input$x2,": ", as.character(data[[1]]), "<br>", input$y2, ": ", as.character(data[[2]]),"<br>")
-    }, "hover")%>%set_options(renderer = "canvas")
   
-})
-
-
-plot3 <- reactive({
-  plotThing <- reactive({
-    data = mtc()
-    data %>%filter((!is.na(data %>% select(match(input$x3, names(data)))) & (!is.na(data %>% select(match(input$y3, names(data))))))) 
+  plot2 <- reactive({
+    plotThing <- reactive({
+      data = mtc()
+      data %>%filter((!is.na(data %>% select(match(input$x2, names(data)))) & (!is.na(data %>% select(match(input$y2, names(data))))))) 
+    })
+    plotThing %>% 
+      ggvis(x=as.name(input$x2), y =as.name(input$y2), key:=~Pmid) %>% 
+      layer_points() %>%
+      add_tooltip(function(data){
+        paste0("Pmid: ", as.character(data$Pmid),"<br>",
+               input$x2,": ", as.character(data[[1]]), "<br>", input$y2, ": ", as.character(data[[2]]),"<br>")
+      }, "hover")%>%set_options(renderer = "canvas")
+    
   })
-  plotThing %>% 
-    ggvis(x=as.name(input$x3), y =as.name(input$y3), key:=~Pmid) %>% 
-    layer_points() %>%
-    add_tooltip(function(data){
-      paste0("Pmid: ", as.character(data$Pmid),"<br>",
-             input$x3,": ", as.character(data[[1]]), "<br>", input$y3, ": ", as.character(data[[2]]),"<br>")
-    }, "hover")%>%set_options(renderer = "canvas")
   
-})
-
-
-plot4 <- reactive({
-  plotThing <- reactive({
-    data = mtc()
-    data %>%filter((!is.na(data %>% select(match(input$x4, names(data)))) & (!is.na(data %>% select(match(input$y4, names(data))))))) 
+  
+  plot3 <- reactive({
+    plotThing <- reactive({
+      data = mtc()
+      data %>%filter((!is.na(data %>% select(match(input$x3, names(data)))) & (!is.na(data %>% select(match(input$y3, names(data))))))) 
+    })
+    plotThing %>% 
+      ggvis(x=as.name(input$x3), y =as.name(input$y3), key:=~Pmid) %>% 
+      layer_points() %>%
+      add_tooltip(function(data){
+        paste0("Pmid: ", as.character(data$Pmid),"<br>",
+               input$x3,": ", as.character(data[[1]]), "<br>", input$y3, ": ", as.character(data[[2]]),"<br>")
+      }, "hover")%>%set_options(renderer = "canvas")
+    
   })
-  plotThing %>% 
-    ggvis(x=as.name(input$x4), y =as.name(input$y4), key:=~Pmid) %>% 
-    layer_points() %>%
-    add_tooltip(function(data){
-      paste0("Pmid: ", as.character(data$Pmid),"<br>",
-             input$x4,": ", as.character(data[[1]]), "<br>", input$y4, ": ", as.character(data[[2]]),"<br>")
-    }, "hover")%>%set_options(renderer = "canvas")
   
-})
-
-
-plot1 %>%bind_shiny("plot1")
-plot2 %>%bind_shiny('plot2')
-plot3 %>%bind_shiny('plot3')
-plot4 %>%bind_shiny('plot4')
+  
+  plot4 <- reactive({
+    plotThing <- reactive({
+      data = mtc()
+      data %>%filter((!is.na(data %>% select(match(input$x4, names(data)))) & (!is.na(data %>% select(match(input$y4, names(data))))))) 
+    })
+    plotThing %>% 
+      ggvis(x=as.name(input$x4), y =as.name(input$y4), key:=~Pmid) %>% 
+      layer_points() %>%
+      add_tooltip(function(data){
+        paste0("Pmid: ", as.character(data$Pmid),"<br>",
+               input$x4,": ", as.character(data[[1]]), "<br>", input$y4, ": ", as.character(data[[2]]),"<br>")
+      }, "hover")%>%set_options(renderer = "canvas")
+    
+  })
+  
+  
+  plot1 %>%bind_shiny("plot1")
+  plot2 %>%bind_shiny('plot2')
+  plot3 %>%bind_shiny('plot3')
+  plot4 %>%bind_shiny('plot4')
 })
--- ui.R
+++ ui.R
@@ -24,7 +24,7 @@ shinyUI(fluidPage(
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
-    sidebarPanel(h5('filter menu here')
+    sidebarPanel(h5('filter menu here'), width = 2
                 
          
        
@@ -32,38 +32,22 @@ shinyUI(fluidPage(
    
     # Show a plot of the generated distribution
     mainPanel(
-      fluidRow(div(style = 'height:"50%";'),
-        column(6, 
-                actionButton('toggle1','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
-                      conditionalPanel(condition = 'input.toggle1 % 2 == 0',
-                                add_input_selector('x1','y1')
-                                ),
-               conditionalPanel('input.toggle1 % 2 == 1', ggvisOutput("plot1"))
-               ),
-               column(3,
-                      actionButton('toggle2','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
-                      conditionalPanel(condition = 'input.toggle2 % 2 == 0',
-                                       add_input_selector('x2','y2')
-                      ),
-                      conditionalPanel('input.toggle2 % 2 == 1', ggvisOutput("plot2"))
+      actionButton('toggle1','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
+       fluidRow(column(12,
+               conditionalPanel(condition = 'input.toggle1 % 2 == 0',
+                                fluidRow(style="padding: 0px 0px 0px 40px;",h2('Select variables to plot')),
+                                fluidRow(column(6,fluidRow(style="padding: 50px 0px 150px 40px;",add_input_selector('x1','y1')), 
+                                fluidRow(style="padding: 0px 0px 0px 40px;",add_input_selector('x2','y2'))),
+                                column(6, fluidRow(style="padding: 50px 0px 150px 40px;",add_input_selector('x3','y3')),
+                                fluidRow(style="padding: 0px 0px 0px 40px;",add_input_selector('x4','y4')))
+                                )),
+               conditionalPanel('input.toggle1 % 2 == 1',
+                                fluidRow(
+                                  column(6, style="padding: 0px 100px 0px 0px;",ggvisOutput("plot1"), ggvisOutput('plot2')),
+                                  column(6, ggvisOutput("plot3"), ggvisOutput('plot4'))
+                                )
                )
-        ),
-      fluidRow(
-        column(6,
-               actionButton('toggle3','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
-               conditionalPanel(condition = 'input.toggle3 % 2 == 0',
-                                add_input_selector('x3','y3')
-               ),
-               conditionalPanel('input.toggle3 % 2 == 1', ggvisOutput("plot3"))
-        ),
-        column(3,
-               actionButton('toggle4','Toggle Show/Edit', icon = icon("cog", lib = "glyphicon")),
-               conditionalPanel(condition = 'input.toggle4 % 2 == 0',
-                                add_input_selector('x4','y4')
-               ),
-               conditionalPanel('input.toggle4 % 2 == 1', ggvisOutput("plot4"))
-        )
       )
-      )
+      ), fluid =TRUE
       
-      )))
+      ))))
