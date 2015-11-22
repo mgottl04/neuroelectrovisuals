5efcae931bf3e36b2630e06de1875a62913844b1
Mike <mikemgottlieb@gmail.com>
2015-11-21 16:47:36 -0800

    Renamed plot functio

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ggvis)
 library(dplyr)
 
-prot <- function(df, x_axis, y_axis){
+make_main_plot <- function(df, x_axis, y_axis){
   data_frame <- df()
   #filt <-
   data_frame %>%  
@@ -40,13 +40,13 @@ shinyServer(function(input, output,session) {
   y4 <- reactive({as.symbol(input$y4)})
   
  
-  reactive({prot(mtc,x1,y1)})%>%
+  reactive({make_main_plot(mtc,x1,y1)})%>%
     bind_shiny('plot1')
-  reactive({prot(mtc,x2,y2)})%>%
+  reactive({make_main_plot(mtc,x2,y2)})%>%
     bind_shiny('plot2')
-  reactive({prot(mtc,x2,y2)})%>%
+  reactive({make_main_plot(mtc,x2,y2)})%>%
     bind_shiny('plot3')
-  reactive({prot(mtc,x4,y4)})%>%
+  reactive({make_main_plot(mtc,x4,y4)})%>%
     bind_shiny('plot4')
 
 })
