1cb1b4b21c24bc6267c2f145b47c0103132f8dba
Mike <mikemgottlieb@gmail.com>
2015-11-21 16:46:18 -0800

    Created make plot function to clean up server.R

--- server.R
+++ server.R
@@ -2,84 +2,51 @@ library(shiny)
 library(ggvis)
 library(dplyr)
 
+prot <- function(df, x_axis, y_axis){
+  data_frame <- df()
+  #filt <-
+  data_frame %>%  
+    filter((!is.na(data_frame[,as.character(x_axis())]))&
+             (!is.na(data_frame[,as.character(y_axis())]))) %>%
+    ggvis(x =x_axis(), y= y_axis() ) %>% 
+    layer_points() %>%
+    add_tooltip(function(data){
+      paste0("Pmid: ", as.character(data$Pmid),"<br>",
+             x_axis(),": ", as.character(data[[1]]), "<br>", y_axis(), ": ", as.character(data[[2]]),"<br>")
+    }, "hover")%>%set_options(renderer = "canvas")
+}
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output,session) {
   
   # Expression that generates a histogram. The expression is
   
-  
+
   mtc <- reactive({
     #global filtering will occur in this reactive
     data = bigData #%>% filter(!is.na(JxnOffset))
     data
   })
   
-  plot1 <- reactive({
-    plotThing <- reactive({
-      data = mtc()
-      data %>%filter((!is.na(data %>% select(match(input$x1, names(data)))) & (!is.na(data %>% select(match(input$y1, names(data))))))) 
-    })
-    plot <- plotThing %>% 
-      ggvis(x=as.name(input$x1), y =as.name(input$y1), key:=~Pmid) %>% 
-      layer_points() %>%
-      add_tooltip(function(data){
-        paste0("Pmid: ", as.character(data$Pmid),"<br>",
-               input$x1,": ", as.character(data[[1]]), "<br>", input$y1, ": ", as.character(data[[2]]),"<br>")
-      }, "hover")%>%set_options(renderer = "canvas")
-    
-  })
-  
-  plot2 <- reactive({
-    plotThing <- reactive({
-      data = mtc()
-      data %>%filter((!is.na(data %>% select(match(input$x2, names(data)))) & (!is.na(data %>% select(match(input$y2, names(data))))))) 
-    })
-    plotThing %>% 
-      ggvis(x=as.name(input$x2), y =as.name(input$y2), key:=~Pmid) %>% 
-      layer_points() %>%
-      add_tooltip(function(data){
-        paste0("Pmid: ", as.character(data$Pmid),"<br>",
-               input$x2,": ", as.character(data[[1]]), "<br>", input$y2, ": ", as.character(data[[2]]),"<br>")
-      }, "hover")%>%set_options(renderer = "canvas")
-    
-  })
+  x1 <- reactive({as.symbol(input$x1)})
+  x2 <- reactive({as.symbol(input$x2)})
+  x3 <- reactive({as.symbol(input$x3)})
+  x4 <- reactive({as.symbol(input$x4)})
   
   
-  plot3 <- reactive({
-    plotThing <- reactive({
-      data = mtc()
-      data %>%filter((!is.na(data %>% select(match(input$x3, names(data)))) & (!is.na(data %>% select(match(input$y3, names(data))))))) 
-    })
-    plotThing %>% 
-      ggvis(x=as.name(input$x3), y =as.name(input$y3), key:=~Pmid) %>% 
-      layer_points() %>%
-      add_tooltip(function(data){
-        paste0("Pmid: ", as.character(data$Pmid),"<br>",
-               input$x3,": ", as.character(data[[1]]), "<br>", input$y3, ": ", as.character(data[[2]]),"<br>")
-      }, "hover")%>%set_options(renderer = "canvas")
-    
-  })
+  y1 <- reactive({as.symbol(input$y1)})
+  y2 <- reactive({as.symbol(input$y2)})
+  y3 <- reactive({as.symbol(input$y3)})
+  y4 <- reactive({as.symbol(input$y4)})
   
-  
-  plot4 <- reactive({
-    plotThing <- reactive({
-      data = mtc()
-      data %>%filter((!is.na(data %>% select(match(input$x4, names(data)))) & (!is.na(data %>% select(match(input$y4, names(data))))))) 
-    })
-    plotThing %>% 
-      ggvis(x=as.name(input$x4), y =as.name(input$y4), key:=~Pmid) %>% 
-      layer_points() %>%
-      add_tooltip(function(data){
-        paste0("Pmid: ", as.character(data$Pmid),"<br>",
-               input$x4,": ", as.character(data[[1]]), "<br>", input$y4, ": ", as.character(data[[2]]),"<br>")
-      }, "hover")%>%set_options(renderer = "canvas")
-    
-  })
-  
-  
-  plot1 %>%bind_shiny("plot1")
-  plot2 %>%bind_shiny('plot2')
-  plot3 %>%bind_shiny('plot3')
-  plot4 %>%bind_shiny('plot4')
+ 
+  reactive({prot(mtc,x1,y1)})%>%
+    bind_shiny('plot1')
+  reactive({prot(mtc,x2,y2)})%>%
+    bind_shiny('plot2')
+  reactive({prot(mtc,x2,y2)})%>%
+    bind_shiny('plot3')
+  reactive({prot(mtc,x4,y4)})%>%
+    bind_shiny('plot4')
+
 })
