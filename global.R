921d46af13b7a73ac9a87e99cf9c7209825079a3
Emily Hindalong <ehindalong@gmail.com>
2015-11-25 13:51:06 -0800

    fixing dummy data

--- global.R
+++ global.R
@@ -13,17 +13,29 @@ bigData[bigData$key,]
 
 # Emily's Dummy Data
 
+TreeViewNode <- function(id,is_leaf,children) {
+  me <- list(id=id,is_leaf=is_leaf,children=children)
+  class(me) <- append(class(me),"TreeViewNode")
+  return(me)
+}
+
+CheckBoxNode <- function(id,is_selected) {
+  me <- list(id=id,is_selected=is_selected)
+  class(me) <- append(class(me),"CheckBoxNode")
+  return(me)
+}
+
 bertha <- CheckBoxNode("bertha",TRUE)
 barb <- CheckBoxNode("barb",FALSE)
 carl <- CheckBoxNode("carl",TRUE)
 craig <- CheckBoxNode("craig",TRUE)
-cunt <- TreeViewNode("cunt",TRUE)
+cunt <- CheckBoxNode("cunt",TRUE)
 
 Bs <- TreeViewNode("Bs",TRUE,list(bertha,barb))
 Cs <- TreeViewNode("Cs",TRUE,list(carl,craig,cunt))
 directory3 <- TreeViewNode("directory3",FALSE,list(Bs,Cs))
 directory2 <- TreeViewNode("directory2",FALSE,list(Bs,Cs))
-directory <- TreeViewNode("directory",TRUE,FALSE,list(directory2,directory3))
+directory <- TreeViewNode("directory",FALSE,list(directory2,directory3))
 
 all_nodes <- list(bertha,barb,Bs,carl,craig,cunt,Cs,directory,directory2,directory3)
 names(all_nodes) = c("bertha","barb","Bs","carl","craig","cunt","Cs","directory","directory2","directory3")
